7df8c307d975b8cc2cfc3a6336521991a7e176fd
Liran Cohen <liran@getwrecked.com>
2020-07-04 22:05:50 -0400

    darwin compile

---
+++ netlink_macos.go
@@ -0,0 +1,12 @@
+// +build darwin
+package netlink
+
+import "github.com/WebP2P/netlink/nl"
+
+// Family type definitions
+const (
+	FAMILY_ALL  = nl.FAMILY_ALL
+	FAMILY_V4   = nl.FAMILY_V4
+	FAMILY_V6   = nl.FAMILY_V6
+	FAMILY_MPLS = nl.FAMILY_MPLS
+)
